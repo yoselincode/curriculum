9f0e246f75bf678c9e4715702bb1b715f4f56812
Piero Nicolini <naturalnen@gmail.com>
2024-10-17 13:30:05 -0300

    update all cv and portafolio sectiones

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+ const lines = ['101010', '110011', '010101', '101101', '111000', '011010', '100100','101010', '110011', '010101', '101101', '111000', '011010', '100100','101010', '110011', '010101', '101101', '111000', '011010', '100100'];
+ const container = document.querySelector('.background-animation');
+
+ function createCodeLine() {
+   const codeLine = document.createElement('div');
+   codeLine.className = 'code-line';
+   codeLine.textContent = lines[Math.floor(Math.random() * lines.length)];
+   
+   codeLine.style.position = 'absolute';
+   codeLine.style.top = Math.random() * 200 + 'vh';
+   codeLine.style.left = Math.random() * 100 + 'vw';
+
+   codeLine.style.animationDelay = Math.random() * 1 + 's';
+
+   container.appendChild(codeLine);
+
+   setTimeout(() => {
+     codeLine.remove();
+   }, 5000);
+ }
+
+ setInterval(createCodeLine, 100);
